769ce4eac0d31337eee18946e4be76d5fe1028f1
konstantinzhuravlev97 <konstantin.zhuravlev.1997@gmail.com>
2026-01-18 01:00:37 +0600

    add slider and footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,38 @@
+import Swiper from "swiper";
+import { Navigation, Pagination} from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "/src/sass/style.scss";
+
+
+try {
+    const swiper = new Swiper('.works__slider', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.icon-right',
+        prevEl: '.icon-left'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    breakpoints: {
+
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1920: {
+            slidesPerView: 3,
+            spaceBetween: 35
+        }
+    }
+    });
+}
+catch (e) {
+
+}

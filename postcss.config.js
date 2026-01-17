5d01bd38984a395d82f8766c63cd6f2bf03354f8
konstantinzhuravlev97 <konstantin.zhuravlev.1997@gmail.com>
2026-01-17 18:07:32 +0600

    add information sections and form

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: {
+		"postcss-pxtorem": {
+			rootValue: 16,
+			propList: ["*"], 
+			selectorBlackList: [], 
+			replace: true, 
+			mediaQuery: true, 
+			minPixelValue: 0, 
+		},
+	},
+};
